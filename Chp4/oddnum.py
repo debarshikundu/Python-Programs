12c08f3b9f892e5f74b482d079e8ed2622217c9c
debarshikundu <debarshikundu0@utexas.edu>
2023-02-17 19:20:44 -0600

    Slicing lists!

--- Chp4/oddnum.py
+++ Chp4/oddnum.py
@@ -5,3 +5,11 @@ for i in range(1,21, 2):
 
 for elem in odd:
     print(elem)
+
+
+print(f"The first three items of the list are: {odd[:3]}")
+
+print(f"Three items from the middle of the list are: {odd[4:7]}")
+
+print(f"Three items from the end of the list are: {odd[-3:]}")
+ 
