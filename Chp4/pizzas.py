7bf7e478efe07a528efae634a8d6a7914eebd70e
debarshikundu <debarshikundu0@utexas.edu>
2023-02-17 19:41:50 -0600

    Updates to exercises 11 and 12!

--- Chp4/pizzas.py
+++ Chp4/pizzas.py
@@ -7,3 +7,20 @@ for pizza in pizzas:
     print(f"I like {pizza} pizza!")
 
 print("I really love pizza!!!")
+
+friend_pizzas = pizzas[:]
+
+pizzas.append("chicken")
+
+friend_pizzas.append("spinach")
+
+print("My favorite pizzas are:")
+
+for pizza in pizzas:
+    print(pizza)
+
+print("My friend's favorite pizzas are:")
+
+for pizza in friend_pizzas:
+    print(pizza)
+
