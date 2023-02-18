fb404b0d50227d1aa4fe994d9f6734e3c1aa3df2
debarshikundu <debarshikundu0@utexas.edu>
2023-02-17 22:04:15 -0600

    test

--- Chp4/buffet.py
+++ Chp4/buffet.py
@@ -1,11 +1,7 @@
 foods = ("pizza", "salad", "apple", "ice cream", "brownies")
-
 for food in foods: 
     print(food)
 
-
 foods = ("pizza", "salad", "apple", "broccoli", "pear")
-
-
 for food in foods:
     print(food)
