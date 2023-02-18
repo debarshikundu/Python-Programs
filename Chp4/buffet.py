177ef16f43a2611550ff05bf5dd1ed0be32abd31
debarshikundu <debarshikundu0@utexas.edu>
2023-02-17 21:47:45 -0600

    Goes over tuples!

---
+++ Chp4/buffet.py
@@ -0,0 +1,11 @@
+foods = ("pizza", "salad", "apple", "ice cream", "brownies")
+
+for food in foods: 
+    print(food)
+
+
+foods = ("pizza", "salad", "apple", "broccoli", "pear")
+
+
+for food in foods:
+    print(food)
