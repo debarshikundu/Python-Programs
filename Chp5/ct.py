44d11e8673e30a890fae0395b91ba7aeea11469a
debarshikundu <debarshikundu0@utexas.edu>
2023-02-18 12:07:33 -0600

    Conditional Tests added!

---
+++ Chp5/ct.py
@@ -0,0 +1,34 @@
+car = "subaru"
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+
+fly = True
+
+print("Do birds fly? I predict True.")
+print(fly == True)
+
+fly = False
+
+print("Do pigs fly? I predict False.")
+print(fly == True)
+
+print("Does 1 == 1? I predict True.")
+print(1 == 1)
+
+print("Does 1 == 2? I predict False.")
+print(1 == 2)
+
+print("Does 5*5 == 25? I predict True.")
+print(5*5 == 25)
+
+print("Does 5*5 == 81? I predict False.")
+print(5*5 == 81)
+
+print("Does 9*9 == 81? I predict True.")
+print(9*9 == 81)
+
+print("Does 9*9 == 356? I predict False.")
+print(9*9 == 356)
