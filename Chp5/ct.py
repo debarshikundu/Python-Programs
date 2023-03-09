08255f3200cf95d988dcf17d068dc5cd78ff8368
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-09 12:52:04 -0600

    First two problems done!

--- Chp5/ct.py
+++ Chp5/ct.py
@@ -32,3 +32,57 @@ print(9*9 == 81)
 
 print("Does 9*9 == 356? I predict False.")
 print(9*9 == 356)
+
+print("Does 'Cat' =  'Cat'? I predict True.")
+print('Cat' == 'Cat')
+
+print("Does 'Cat' = 'Dog'? I think not." )
+print('Cat' == 'Dog')
+
+print("Does 'cat' = 'Cat'.lower())? Yep!")
+print( 'cat' == 'Cat'.lower())
+
+print("Does 'Dog' = 'Dog'.lower()? No way!")
+print('Dog' == 'Dog'.lower())
+
+print("Is 5 != 2? True!")
+print(5 != 2)
+
+print("Is 5 != 5? False!")
+print(5 != 5)
+
+print("Is 5 > 5? False!")
+print(5 > 5)
+
+print("Is 5 >= 5? True!")
+print(5 >= 5)
+
+print("Is 4 < 5? True!")
+print(4 < 5)
+
+print("Is 4 <= 5? True!")
+print(4 <= 5)
+
+print("Two Trues = True")
+print(True and True)
+
+print("One True and One False = False")
+print(True and False)
+
+print("One True or one False = True")
+print(True or False)
+
+print("False or False = False")
+print(False or False)
+
+print("Is 'jam' in the list of jellies? Yes!")
+print('jam' in ['Jelly', 'Jelly', 'jam'])
+
+print("Is 'Pluto' a planet? No!")
+print('Pluto' in ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"])
+
+print("Is 3 not in the list? Yes!")
+print(3 not in [1, 2, 4, 5])
+
+print("Is 4 not in the list? No!")
+print(4 not in [1,2,4,5])
