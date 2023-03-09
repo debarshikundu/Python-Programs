f8c5872cc4ca0ed0cf2d279ccf2d78a1d94704da
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-09 13:50:06 -0600

    Favorite Fruits added!

---
+++ Chp5/favorite_fruits.py
@@ -0,0 +1,13 @@
+favorite_fruits = ["Apple", "Grapes", "Mango"]
+
+if "Apple" in favorite_fruits:
+    print("You really like apples!")
+if "Grapes" in favorite_fruits:
+    print("You really like grapes!")
+if "Mango" in favorite_fruits:
+    print("You really like mangos!")
+if "Pomegranate" in favorite_fruits:
+    print("Wrong!")
+if "Hello" in favorite_fruits: 
+    print("That is not even a fruit!")
+
