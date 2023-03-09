3947ff6fcda26bfe485d51113978286c0cc2036d
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-09 14:14:09 -0600

    hello_admin tweaked a bit...

--- Chp5/hello_admin.py
+++ Chp5/hello_admin.py
@@ -1,7 +1,10 @@
-usernames = ["admin", "rosetta", "debarshi", "strawHide", "driver"]
+usernames = []#["admin", "rosetta", "debarshi", "strawHide", "driver"]
 
-for user in usernames:
-    if user == 'admin':
-        print(f"Hello {user}, would you like to see a status report?")
-    else:
-        print(f"Hello {user}, thank you for logging in again.")
+if usernames:
+    for user in usernames:
+        if user == 'admin':
+            print(f"Hello {user}, would you like to see a status report?")
+        else:
+            print(f"Hello {user}, thank you for logging in again.")
+else:
+    print("We need to find some users!")
