f49fbb68ca5a74f1594706aeec0ac89835c6657c
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-09 13:22:49 -0600

    Final alien_colors done!!!

--- Chp5/alien_colors.py
+++ Chp5/alien_colors.py
@@ -2,14 +2,26 @@ alien_color = 'green'
 
 if alien_color == 'green':
     print("You just earned five points!")
-else:
+elif(alien_color == 'yellow'):
     print("You just earned ten points!")
-
+else:
+    print("You just earned fifteen points")
 
 alien_color = 'yellow'
 
 if alien_color == 'green':
     print("You just earned five points!")
-else:
+elif(alien_color == 'yellow'):
     print("You just earned ten points!")
+else:
+    print("You just earned fifteen points")
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned five points!")
+elif(alien_color == 'yellow'):
+    print("You just earned ten points!")
+else:
+    print("You just earned fifteen points")
 
