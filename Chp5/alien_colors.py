2134866d339c942f3235221e0fa4ea7448349075
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-09 13:13:00 -0600

    if-else added

---
+++ Chp5/alien_colors.py
@@ -0,0 +1,15 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned five points!")
+else:
+    print("You just earned ten points!")
+
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned five points!")
+else:
+    print("You just earned ten points!")
+
