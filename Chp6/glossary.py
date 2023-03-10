6f93fd6a6ed327ad1e8fde317cdfa8f75e583350
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-10 13:29:42 -0600

    Favorite Numbers added

---
+++ Chp6/glossary.py
@@ -0,0 +1,17 @@
+terms = {
+        "Conditional Test": "A comparison between two values.",
+        "Data":"the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media.",
+        "Dictionary": "A collection of key-value pairs.",
+         "Float": "Decimal values or fractional numbers.",
+         "Integer":"Numbers without a fractional component, and don't support decimal points.",
+         "Key": "The first item in a key-value pair in a dictionary.",
+         "List": "A collection of items in a particular order.",
+         "Memory":"Temporary space in a computer used for storing operations and program data.",
+         "Value": "An item associated with a key in a dictionary.",
+         "Variable":"A value that can change, depending on conditions or on information passed to the program."
+         }
+
+
+for k,v in terms.items():
+    print(k)
+    print(v)
