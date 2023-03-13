47ccc47fa8d8a5d926c5f309a0f5245780f5a079
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-13 00:21:56 -0500

    final changes done!

--- Chp7/movie_tickets.py
+++ Chp7/movie_tickets.py
@@ -1,15 +1,17 @@
 
 age = 0
-
-while age != -1:
+active = True 
+while age != -1 and active != False:
     age = int(input("What is your age? Enter '-1' to exit.\n"))
     
-    if age != -1:
-        if age < 3 and age >= 0:
-            print("Your ticket is free!")
-        elif age >= 3 and age <= 12:
-            print("Your ticket is 10 dollars.")
-        elif age > 12:
-            print("Your ticket is 15 dollars.")
-        else:
-            print("Invalid Number for age...")
+    if age == -1:
+        active = False 
+        break
+    elif age < 3 and age >= 0:
+        print("Your ticket is free!")
+    elif age >= 3 and age <= 12:
+        print("Your ticket is 10 dollars.")
+    elif age > 12:
+        print("Your ticket is 15 dollars.")
+    else:
+        print("Invalid Number for age...")
