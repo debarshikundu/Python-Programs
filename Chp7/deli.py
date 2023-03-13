1239d91b3bd25fe5f27e42ddfbb93cafc2df808e
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-13 00:52:50 -0500

    final changes done!

--- Chp7/deli.py
+++ Chp7/deli.py
@@ -1,9 +1,13 @@
-sandwhich_orders = ["Tuna", "Grilled Cheese", "Club"]
+sandwhich_orders = ["Tuna", "Grilled Cheese", "Club", "Pastrami", "Pastrami", "Pastrami"]
 
 finished_sandwhiches = []
 
+print("The Deli has run out of Pastrami!!!")
 for sandwhich in sandwhich_orders:
-    print(f"I made you a {sandwhich} sandwhich.")
+    if sandwhich != "Pastrami":
+        print(f"I made you a {sandwhich} sandwhich.")
+    else:
+        print("The Deli has run out of Pastrami!!!")
     finished_sandwhiches.append(sandwhich)
 print("All sandwhich orders have been fulfilled!")
         
