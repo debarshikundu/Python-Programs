b2b5a6e86655c3f599a8d6459c09b993460cb706
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-03-13 00:43:03 -0500

    final changes done!

---
+++ Chp7/deli.py
@@ -0,0 +1,9 @@
+sandwhich_orders = ["Tuna", "Grilled Cheese", "Club"]
+
+finished_sandwhiches = []
+
+for sandwhich in sandwhich_orders:
+    print(f"I made you a {sandwhich} sandwhich.")
+    finished_sandwhiches.append(sandwhich)
+print("All sandwhich orders have been fulfilled!")
+        
