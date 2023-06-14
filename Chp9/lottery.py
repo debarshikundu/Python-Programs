0663ffeec3770538679260cb634d49f6c63e69ab
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-06-13 21:44:14 -0500

    Added lottery.py

---
+++ Chp9/lottery.py
@@ -0,0 +1,16 @@
+from random import randint
+
+list = [6, 3, 72, 634, 1234, "a", "d", "h", "r", "t", 4, 5343, 6, 234, 7343]
+
+print("Any Ticket matching these four numbers or letters, wins a prize!!!")
+
+winners = []
+
+def selectIndex():
+    index = randint(0, len(list)-1)
+    return list[index]
+
+for i in range(4):
+    print(selectIndex())
+
+
