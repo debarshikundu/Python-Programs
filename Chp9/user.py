4f8e32d4a114a5be0dd9f7d16ed5dee8101c8302
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-06-13 20:14:22 -0500

    testing login attempts

--- Chp9/user.py
+++ Chp9/user.py
@@ -9,20 +9,24 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.dob = dob 
+        self.login_attempts = 0
     def describe_user(self):
         print(f"{self.first_name} {self.last_name}- Date of Birth: {self.dob}")
     def greet_user(self):
         print(f"Hello {self.first_name} {self.last_name}!")
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 user1 = User("Rosario", "Rodriguez", "09/07/03")
-user2 = User("Lucy", "Musimo", "04/16/05")
-user3 = User("Tracy", "Mellencamp", "02/02/01")
 
-user1.describe_user()
-user1.greet_user()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
 
-user2.describe_user()
-user2.greet_user()
+print(user1.login_attempts)
 
-user3.describe_user()
-user3.greet_user()
+user1.reset_login_attempts()
+
+print(user1.login_attempts)
