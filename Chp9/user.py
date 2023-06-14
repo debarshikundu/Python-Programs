47dac1a017221ae7152127b243aec66906bfe56d
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-06-13 20:47:46 -0500

    Added Privilege class

--- Chp9/user.py
+++ Chp9/user.py
@@ -19,14 +19,17 @@ class User:
     def reset_login_attempts(self):
         self.login_attempts = 0
 
-user1 = User("Rosario", "Rodriguez", "09/07/03")
+class Admin(User):
+    def __init__(self, first_name, last_name, dob):
+        super().__init__(first_name, last_name, dob)
+        self.privileges = Privileges([["can add post", "can delete post", "can ban user"]])
+    
+class Privileges:
+    def __init__(self, privileges=[]) -> None:
+        self.privileges = privileges
+    def show_privileges(self):
+        for privilege in self.privileges:
+            print(privilege)   
 
-user1.increment_login_attempts()
-user1.increment_login_attempts()
-user1.increment_login_attempts()
-
-print(user1.login_attempts)
-
-user1.reset_login_attempts()
-
-print(user1.login_attempts)
+eric = Admin("Eric", "Sanchez", "04/04/99")
+eric.privileges.show_privileges()
