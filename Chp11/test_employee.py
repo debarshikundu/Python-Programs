675199373afaa4c5095d15e0e4ffc556e02c1c98
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-06-15 15:57:27 -0500

    Testing using fixtures...

--- Chp11/test_employee.py
+++ Chp11/test_employee.py
@@ -1,12 +1,19 @@
+import pytest
 from employee import Employee
+
+@pytest.fixture
+def bobOrSally():
+    """Employee Object that is available to all test functions"""
+    bobOrSally = Employee('Debarshi', 'Kundu', 60000)
+    return bobOrSally
+
+
 """ Testing Employee class give_raise() function"""
 
-def test_give_default_raise():
-    e = Employee('Debarshi', 'Kundu', 60000)
-    e.give_raise()
-    assert e.asalary == 65000
+def test_give_default_raise(bobOrSally):
+    bobOrSally.give_raise()
+    assert bobOrSally.asalary == 65000
 
-def test_give_custom_raise():
-    e = Employee('Debarshi', 'Kundu', 60000)
-    e.give_raise(20000)
-    assert e.asalary == 80000
+def test_give_custom_raise(bobOrSally):
+    bobOrSally.give_raise(20000)
+    assert bobOrSally.asalary == 80000
