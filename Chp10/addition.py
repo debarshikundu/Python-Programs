d41c4bebd80037e5537abc8a806afb1c24d5ffb4
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-06-14 21:21:53 -0500

    copied this part... it is boring

--- Chp10/addition.py
+++ Chp10/addition.py
@@ -1,11 +1,22 @@
-try:
-    x = input("Enter a number: ")
-    x = int(x)
+print("Enter 'q' at any time to quit.\n")
 
-    y = input("Enter another number: ")
-    y = int(y)
-except ValueError:
-    print("Sorry, enter a number, not anything else...")
-else:
-    sum = x + y 
-    print(f"The sum of {x} and {y} is {sum}.")
+while True:
+    try:
+        x = input("\nGive me a number: ")
+        if x == 'q':
+            break
+
+        x = int(x)
+
+        y = input("Give me another number: ")
+        if y == 'q':
+            break
+
+        y = int(y)
+
+    except ValueError:
+        print("Sorry, I really needed a number.")
+
+    else:
+        sum = x + y
+        print(f"The sum of {x} and {y} is {sum}.")
