7835ef334329b0ce14747ab7c2f54ba13939c61b
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-06-14 20:12:28 -0500

    learning_python files

---
+++ Chp10/learning_python.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+"""Absolute or relative path to file..."""
+path = Path('/home/debarshi/Desktop/Python-Programs/Chp10/learning_python.txt')
+#Reads entire file 
+contents = path.read_text()
+#prints out entire file
+print(contents)
+#splits lines into list
+lines = contents.splitlines()
+#prints each line on a new line
+for line in lines: 
+    print(line)
