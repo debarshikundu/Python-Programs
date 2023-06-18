889512e0b54e2b0109516b56b386080f0bf99ff6
Debarshi Kundu <debarshikundu0@utexas.edu>
2023-06-18 16:29:47 -0500

    First two chapter 12 exercises...

---
+++ Chp12/scratch_cat.py
@@ -0,0 +1,18 @@
+import pygame
+
+import pygame
+ 
+class Cat:
+ 
+    def __init__(self, bb_game):
+        self.screen = bb_game.screen
+        self.screen_rect = bb_game.screen.get_rect()
+
+       
+        self.image = pygame.image.load('200px-ScratchCat-Small.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.center = self.screen_rect.center
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
